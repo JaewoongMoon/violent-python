a9b4fca082836fcac6ac6d7dba58645cea9d2da8
root <root@localhost.localdomain>
2018-07-11 03:12:45 +0900

    fix indent again

--- portscanner.py
+++ portscanner.py
@@ -3,35 +3,35 @@ from socket import *
 from threading import *
 screenLock = Semaphore(value=1)
 def connScan(tgtHost, tgtPort):
-	try:
-		connSkt = socket(AF_INET, SOCK_STREAM)
-		connSkt.connect((tgtHost, tgtPort))
-		connSkt.send("ViolentPython\r\n")
-		results = connSkt.recv(100)
-		screenLock.acquire()   
-		print '[+]%d/tcp open'% tgtPort
-        print '[+] ' + str(results)
-		
-	except:
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        connSkt.send("ViolentPython\r\n")
+        results = connSkt.recv(100)
         screenLock.acquire()
-		print '[-]%d/tcp closed'% tgtPort
+        print '[+]%d/tcp open'% tgtPort
+        print '[+] ' + str(results)
+    except:
+        screenLock.acquire()
+        print '[-]%d/tcp closed'% tgtPort
     finally:
         screenLock.release()
         connSkt.close()
 
 def portScan(tgtHost, tgtPorts):
-	try:
+    try:
 		tgtIP = gethostbyname(tgtHost)
-	except:
+    except:
 		print "[-] Cannot resolve '%s': Unknown host"% tgtHost
 		return
-	try:
+    try:
 		tgtName = gethostbyaddr(tgtIP)
 		print '\n[+] Scan Results for: ' + tgtName[0]
-	except:
+    except:
 		print '\n[+] Scan Results for: ' + tgtIP
-	setdefaulttimeout(1)
-	for tgtPort in tgtPorts:
+
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
         t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
         t.start()
 
